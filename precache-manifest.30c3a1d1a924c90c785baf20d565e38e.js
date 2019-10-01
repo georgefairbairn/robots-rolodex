7a9cdcc67a721af54327cb3744704b214f5f2043
George Fairbairn <george.fairbairn@Georges-MacBook-Pro.local>
2019-09-30 21:22:55 -0700

    Updates

---
+++ precache-manifest.30c3a1d1a924c90c785baf20d565e38e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00cb52bb4a412636aa57ef7d3072c47",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "18db62434943a4882d05",
+    "url": "/robots-rolodex/static/css/main.d101c745.chunk.css"
+  },
+  {
+    "revision": "2f5af314df5d289dde96",
+    "url": "/robots-rolodex/static/js/2.2b5aaf86.chunk.js"
+  },
+  {
+    "revision": "18db62434943a4882d05",
+    "url": "/robots-rolodex/static/js/main.ec2bb882.chunk.js"
+  },
+  {
+    "revision": "52e236d3b22f28ca4acb",
+    "url": "/robots-rolodex/static/js/runtime-main.c416b7b9.js"
+  }
+]);
